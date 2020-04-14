9a9cb6d7f9407d13d336764ba2a6b47ed3f8c98c
Natan <Natan@DESKTOP-BF3LPKV>
2020-04-13 21:31:57 -0300

    Trabalho de Sistemas Operacionais I

---
+++ src/Controller/Jogador.java
@@ -0,0 +1,57 @@
+package Controller;
+
+import java.util.Random;
+
+public class Jogador extends Thread {
+	static int ContadorVitorias;
+	private int dado1, dado2, numeroThread, numeroVencedores;
+	Random aleatorio = new Random();
+	
+	public Jogador(int numeroThread) {
+		start();
+		this.numeroThread = numeroThread;
+	}
+	
+	public void run() {
+		checaDados();
+	}
+	
+	public void checaDados() {
+		int pontoGanho = 0;
+		do {
+			try {
+				sleep(100);
+			} catch (InterruptedException e) {
+				System.err.println("Thread interrompida!");
+			}
+			dado1 = aleatorio.nextInt(6) + 1;
+			dado2 = aleatorio.nextInt(6) + 1;
+			if(dado1 + dado2 == 7 || dado1 + dado2 == 11) {
+				pontoGanho++;
+			}
+		} while(pontoGanho != 5);
+		ContadorVitorias++;
+		if(ContadorVitorias < 4) {
+			switch(ContadorVitorias) {
+			case 1:
+				System.out.println("Jogador #" + numeroThread + " ficou em primeiro e ganhou 5000 reais! Tempo = " + (fim - inicio) + " millisegundos");
+				break;
+			case 2:
+				System.out.println("Jogador #" + numeroThread + " ficou em segundo e ganhou 4000 reais! Tempo = " + (fim - inicio) + " millisegundos");
+				break;
+			case 3:
+				System.out.println("Jogador #" + numeroThread + " ficou em terceiro e ganhou 3000 reais! Tempo = " + (fim - inicio) + " millisegundos");
+				break;
+			}
+			
+		}
+		
+	}
+	}
+	
+	
+	
+	
+	//Preciso fazer de uma forma que eu saiba quem terminou primeiro
+	
+
---
+++ src/View/Principal.java
@@ -0,0 +1,12 @@
+package View;
+
+import Controller.Jogador;
+
+public class Principal {
+	public static void main(String[] args) {
+		for(int i = 1;i <= 10;i++) {
+			Jogador j1 = new Jogador(i);
+		}
+		
+	}
+}
